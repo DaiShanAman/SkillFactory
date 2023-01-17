579a21f47727355b81edb0c53839b8e03f61f3e7
Ishamael <rikonvald@gmail.com>
2023-01-17 17:21:24 +0300

    5.5.4-5.5.8

--- SkillFactory/Module 5/Taks_5_5.cs
+++ SkillFactory/Module 5/Taks_5_5.cs
@@ -11,6 +11,9 @@
             var deep = int.Parse(Console.ReadLine());
 
             Echo(phrase, deep);
+            
+            var factorial20 = Factorial(20);
+            Console.WriteLine(factorial20);
 
             Console.ReadKey();
         }
@@ -24,11 +27,40 @@
                 modif = modif.Remove(0, 2);
             }
 
+            Console.BackgroundColor = (ConsoleColor)deep;
             Console.WriteLine("..." + modif);
 
             if (deep > 1)
             {
                 Echo(modif, deep - 1);
+            }
+        }
+
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+        static int PowerUp(int N, byte pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else if (pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                pow -= 1;
+                return N * PowerUp(N, pow);
             }
         }
     }
