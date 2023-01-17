724fd248a119925903ff33e25fdd40a236984cb2
Ishamael <rikonvald@gmail.com>
2023-01-17 14:52:54 +0300

    5.5.3

---
+++ SkillFactory/Module 5/Taks_5_5.cs
@@ -0,0 +1,35 @@
+﻿namespace SkillFactory.Module_5
+{
+    class Taks_5_5
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Напишите что-то");
+            var phrase = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(phrase, deep);
+
+            Console.ReadKey();
+        }
+
+        static void Echo(string saidworld, int deep)
+        {
+            var modif = saidworld;
+
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+
+            Console.WriteLine("..." + modif);
+
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+        }
+    }
+}
