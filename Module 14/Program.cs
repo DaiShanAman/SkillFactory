6d1252a3866897f3c3aed0c42ce69876985d3e33
Rick <rikonvald@gmail.com>
2023-05-15 17:57:24 +0300

    ***

--- Module 14/Program.cs
+++ Module 14/Program.cs
@@ -4,14 +4,15 @@
     {
         static void Main(string[] args)
         {
-            var phoneBook = new List<Contact>();
-
-            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
-            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
-            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
-            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
-            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
-            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+            var phoneBook = new List<Contact>
+            {
+                new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"),
+                new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"),
+                new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"),
+                new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"),
+                new Contact("Сергей", "Брин", 799900000013, "serg@example.com"),
+                new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com")
+            };
 
             var sortedPhoneBook = phoneBook.OrderBy(p => p.Name).ThenBy(p => p.LastName);
 
