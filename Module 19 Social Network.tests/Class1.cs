ab11174a21ad59f6ad39fd237405cfea8701fddf
Rick <rikonvald@gmail.com>
2023-11-10 11:14:45 +0300

    test

---
+++ Module 19 Social Network.tests/Class1.cs
@@ -0,0 +1,37 @@
+﻿using Module_19_Social_Network.BLL.Services;
+using Module_19_Social_Network.DAL.Entities;
+using Module_19_Social_Network.DAL.Repositories;
+using NUnit.Framework;
+
+namespace Module_19_Social_Network.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void UserService_CreateUser_ShouldCreateUserWithData()
+        {
+            // Arrange
+            var userRepository = new UserRepository();
+            var userService = new UserService();
+            var uzver = new UserEntity
+            {
+                id = 5, // id
+                firstname = "Rick", // name
+                lastname = "Rick", // lastname
+                password = "asswordpassword", // password
+                email = "therick@lol.com", // email
+            };
+
+
+
+            userRepository.Create(uzver);
+
+            var find = userRepository.FindById(uzver.id);
+
+            // Assert
+            Assert.IsTrue(find != null);
+        }
+
+    }
+}
